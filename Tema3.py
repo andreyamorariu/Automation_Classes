2293552717115430612d732744f83b17eb579577
Andreea Morariu <101407348+andreyamorariu@users.noreply.github.com>
2022-03-12 20:43:46 +0200

    Update Tema3.py

--- Tema3.py
+++ Tema3.py
@@ -1,5 +1,3 @@
-#b. Usor spre Mediu (obligatoriu)
-
 #1.
 #Declara o lista note_muzicale in care sa pui do re mi etc pana la do
 #Afiseaz-o
@@ -156,27 +154,7 @@
 
 #print(zile_sapt.intersection(weekend))
 
-#c. Optionale (may need google)
-#16.
-#Ne imaginam o echipa de fotbal pt teren sintetic.
-#3 Schimbari maxime admise
 
-#Declara o Lista cu 5 jucatori
-#Schimbari_efectuate = va jucati voi cu valori diferite
-
-#Daca Jucatorul x e in teren si mai avem schimbari la dispozitie
-#Efectuam schimbarea
-#Stergem jucatorul scos din lista
-#Adaugam jucatorul intrat
-#Afisam a intra x, a iesit y, mai aveti z schimbari
-#Daca jucatorul nu e in teren:
-#Afisati ‘ nu se poate efectua schimbarea deoarece jucatorul x nu e in teren’
-#Afisati ‘mai aveti z schimbari’
-
-#Testati codul cu diferite valori
-
-#Google search hint
-#“how to check if item is in list python”
 
 
 
